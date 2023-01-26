a9001df5049db6be778307122fe4c4ff8d50729e
Samantha Sargent <samjsargent07@gmail.com>
2023-01-26 12:55:42 -0800

    Add calling methods project

---
+++ CallingMethods/Maths.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CallingMethods
+{
+    public class Maths
+    {
+        // Method that takes user input number and multiplies by 2
+        public static void MultiplyBy2(int userNum)
+        {
+            Console.WriteLine(userNum * 2);
+        }
+
+        // Method that takes user input number and adds 20
+        public static void Add20(int userNum)
+        {
+            Console.WriteLine(userNum + 20);
+        }
+
+        // Method that takes user input number and divides by 3
+        public static void DivideBy3(int userNum)
+        {
+            Console.WriteLine(userNum / 3);
+        }
+    }
+}
---
+++ CallingMethods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number do you want to use?");
+            // Gets text input by user and converts it to an integer
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The results are:");
+
+            // Calls methods from Maths.cs and uses them with the number input by the user
+            Maths.MultiplyBy2(userNum); 
+            Maths.Add20(userNum);
+            Maths.DivideBy3(userNum);
+
+            Console.ReadLine();
+        }
+    }
+}
