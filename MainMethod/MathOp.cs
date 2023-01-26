0890b829cc000dc7b86f0e8df52a0654401e86b2
Samantha Sargent <samjsargent07@gmail.com>
2023-01-26 13:53:13 -0800

    Added main method project

---
+++ MainMethod/MathOp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MainMethod
+{
+    public class MathOp
+    {
+        public static int DoMath(int userInt)
+        {
+            // Method takes user input number (as an integer)
+            // Multiplies it 10
+            // Returns the result
+            int product = userInt * 10;
+            return product;
+        }
+
+        public static int DoMath(decimal userDecimal)
+        {
+            // Method takes user input number (as a decimal)
+            // Adds 20 to it
+            // Result is converted to an integer
+            // Returns the result
+            decimal sum = userDecimal + 20.0m;
+            int sumInt = Convert.ToInt32(sum);
+            return sumInt;
+        }
+
+        public static int DoMath(string userStrNum)
+        {
+            // Method takes user input number (as a string)
+            // Converts the string to an integer (if possible)
+            // Divides it by 2
+            // Returns the result
+            int userConvertNum = Convert.ToInt32(userStrNum);
+            int quotient = userConvertNum / 2;            
+            return quotient;
+        }
+    }
+}
---
+++ MainMethod/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            // Takes number user entered and converts it to an integer
+            int userInt = Convert.ToInt32(Console.ReadLine());
+            // Gets the result from the first "DoMath" method (user number times 10)
+            int productResult = MathOp.DoMath(userInt);
+            // Outputs the result from the first Method
+            Console.Write("Your number times 10 equals: {0} \n", productResult);
+
+            Console.WriteLine("Enter a number:");
+            // Takes number user entered and converts it to a decimal
+            decimal userDecimal = Convert.ToDecimal(Console.ReadLine());
+            // Gets the result from the second "DoMath" method (user number plus 20)
+            int sumResult = MathOp.DoMath(userDecimal);
+            // Outputs the result from the second Method
+            Console.Write("Your number plus 20 equals: {0} \n", sumResult);
+
+            Console.WriteLine("Enter a number:");
+            // Takes number user entered in as a string
+            string userStrNum = Console.ReadLine();
+            // Gets the result from the third "DoMath" method (user number divided by 2)
+            int quotResult = MathOp.DoMath(userStrNum);
+            // Outputs the result from the third Method
+            Console.Write("Your number divided by 2 equals: {0} \n", quotResult);
+
+            Console.ReadLine();
+        }
+    }
+}
