d4a85b0a2c08e2820d9924e8dbd15b3c5225b311
Samantha Sargent <samjsargent07@gmail.com>
2023-01-27 11:38:13 -0800

    Added two integer method project

---
+++ MethodTwo/MathOp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodTwo
+{
+    public class MathOp
+    {   
+        // Method AddTogether takes two integers
+        // The second one is optional, and the default value is 5
+        public int AddTogether(int userNum1, int userNum2 = 5)
+        {
+            // Takes userNum1 and userNum2, adds them together and then multiplies by 2
+            int result = (userNum1 + userNum2) * 2;
+            return result;
+        }
+    }
+}
---
+++ MethodTwo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MethodTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiates the class MathOp, where the method that will be used is
+            MathOp maths = new MathOp();
+
+            // Asks user for first number, and converts it to an integer
+            Console.WriteLine("Please enter a number.");
+            int userNum1 = Convert.ToInt32(Console.ReadLine());
+            
+            // Asks user for second number, which is optional
+            // Logs this as a string
+            Console.WriteLine("Please enter a second number. (This is optional.)");
+            string userString = Console.ReadLine();
+
+            // Sets up variable 'result' so it can be used in the if/else statement
+            // This is where the result of the equation will go
+            int result = 0;
+
+            // If userString contains "" or is null,
+            // The method will use the default value 
+            if (string.IsNullOrEmpty(userString))
+            {
+                result = maths.AddTogether(userNum1);
+            }
+            // Otherwise, the number entered into userString is converted to an integer
+            // And used in the method with the first number entered
+            else
+            {
+                int userNum2 = Convert.ToInt32(userString);
+                result = maths.AddTogether(userNum1, userNum2);
+            }
+
+            // Writes the result of the equation
+            Console.WriteLine("The result is {0}.", result);
+            Console.ReadLine();
+        }
+    }
+}
