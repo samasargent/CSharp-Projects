0c757c78ba6af15d4812f8912beb03df50c04bb1
Samantha Sargent <samjsargent07@gmail.com>
2023-01-27 11:56:05 -0800

    Added method class project

---
+++ MethodClass/MathOp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MethodClass
+{
+    public class MathOp
+    {   
+        // Method TwoNumbers takes two integers
+        public void TwoNumbers(int num1, int num2)
+        {   
+            // Does a math operation (multiplication) on the first integer
+            int product = num1 * 3;
+            // Displays the second integer to the screen
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ MethodClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiates the class MathOp, where the method is
+            MathOp mathProblem = new MathOp();
+            
+            // Calling the method, passing in two numbers
+            mathProblem.TwoNumbers(2, 3);
+
+            // Calling the method with parameters by name
+            mathProblem.TwoNumbers(num1: 5, num2: 12);
+
+            Console.ReadLine(); 
+        }
+    }
+}
