39261ee450a8f9fd493a2a72e3a31f3345f8041e
Samantha Sargent <samjsargent07@gmail.com>
2023-01-27 14:54:38 -0800

    Added class project

---
+++ Class/MathOp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassProject
+{
+    public class MathOp
+    {
+        // Void method that outputs an integer with output parameterse
+        public void DoMath(int userNum, out int result)
+        {
+            result = userNum / 2;
+        }
+
+        // Overloading the DoMath method, also making it static
+        public static int DoMath(int userNum)
+        {
+            int result = (userNum + userNum);
+            return result;
+        }
+    }
+}
---
+++ Class/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating the class where our methods are
+            MathOp mathProblem = new MathOp();
+
+            // Getting the user to enter a number and converting it to an integer
+            Console.WriteLine("Please enter a number.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            // Using the first DoMath, with an out parameter
+            mathProblem.DoMath(userNum, out int result);
+            Console.WriteLine("The value of your number divided by 2 is {0}.", result);
+
+            // Using the static method, the second DoMath
+            int result2 = MathOp.DoMath(userNum);
+            Console.WriteLine("The value of your number plus itself is {0}.", result2);
+
+            Console.ReadLine();
+        }
+    }
+}
