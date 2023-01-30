f973db05e1fef06fa47d691b41ce5b1e70d591f9
Samantha Sargent <samjsargent07@gmail.com>
2023-01-30 14:59:02 -0800

    Added overloading operators project

---
+++ Operators/Employee.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    public class Employee
+    {
+        // Setting up properties for ID and first and last name
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // In order to overload == and !=, we need to also overload Equals() and GetHashCode()
+        public override bool Equals(object employee)
+        {
+            // If parameter is null, return false
+            if (employee == null) 
+            { return false; }
+
+            // If paramater is not the same type, return faslse
+            if (employee.GetType() != typeof(Employee))
+            { return false; }
+            
+            var employee1 = (Employee)employee;
+            // Return true if fields match
+            return Id == employee1.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();
+        }
+
+        // Overloading == to check if the two employee IDs are equal to each other
+        public static bool operator ==(Employee employeeLeft, Employee employeeRight)
+        {
+            // If yes, they are the same, return true
+            if (employeeLeft.Id == employeeRight.Id)
+            {
+                return true;
+            }
+            // If no, they are not the same, return false
+            else
+            {
+                return false;
+            }
+        }
+        
+        // Overloading !=, because we have to if we are overloading ==
+        // Checks if the employee IDs are NOT equal to each other
+        public static bool operator !=(Employee employeeLeft, Employee employeeRight)
+        {
+            // If yes, they are not the same, return false
+            if (employeeLeft.Id != employeeRight.Id)
+            {
+                return false;
+            }
+            // If no, they are the same, return true
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ Operators/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating two Employee objects  
+            Employee employee1 = new Employee() { Id = 001, FirstName = "John", LastName = "Smith" };
+            Employee employee2 = new Employee() { Id = 002, FirstName = "Jake", LastName = "Jones" };
+
+            // Using overloaded == to check if the two employees have the same ID
+            Console.WriteLine(employee1 == employee2 ? "Both employees have the same id." : "These are two different employees.");
+
+            // Using overloaded != to check if the two employees have different IDs
+            Console.WriteLine(employee1 != employee2 ? "These employees have the same id." : "These employees are not the same."); 
+            
+            Console.ReadLine();
+        }
+    }
+}
