04669fb335060ca498a4fec09f563a428519e883
Samantha Sargent <samjsargent07@gmail.com>
2023-01-31 14:21:32 -0800

    Added struct project

---
+++ Struct/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        // Creating a struct called Number with the property Amount of data type decimal
+        struct Number
+        {
+            public decimal Amount;
+        }
+
+        static void Main(string[] args)
+        {
+            // Creating a Number object "money" with Amount equal to 15.00
+            Number money = new Number() { Amount = 15.00m };
+
+            // Writing Amount to screen
+            Console.WriteLine(money.Amount);
+            Console.ReadLine();
+        }
+    }
+}
