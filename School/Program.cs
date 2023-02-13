f5759b63380f549bb51adafee4bd9874fd6a2ad7
Samantha Sargent <samjsargent07@gmail.com>
2023-02-13 14:28:45 -0800

    Added students project

---
+++ School/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var context = new SchoolContext()) 
+            {
+                var stud = new Student() { StudentName = "Joe Smith" };
+
+                context.Students.Add(stud);
+                context.SaveChanges();
+            }
+        }
+    }
+}
